4a7b28fc53d475352a2fe18b90739247a267aca0
Denis Klemeshov <denis.klemeshov@gmail.com>
2021-10-18 16:50:13 +0300

    feat: stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JlvjTLDFpK8GWRoFv8CfwAeeSfzzaAJmvsMRH4Ik0G01DfEOtBC3q6xnvzztkMpILmCxQBziD0tGeJFAQaT9ZDo0026XCeFTe';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Clothing Shop'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
