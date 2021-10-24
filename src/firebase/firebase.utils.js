320fc889f4c563bb2a6ee225c03d4ad6ed8b5ab8
Denis Klemeshov <denis.klemeshov@gmail.com>
2021-10-24 15:57:44 +0300

    feat: add HOC, styled-components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFgki31LNTHLuW8sutx-DZo6YAUXWo0pk",
-  authDomain: "ecom-db-store.firebaseapp.com",
-  projectId: "ecom-db-store",
-  storageBucket: "ecom-db-store.appspot.com",
-  messagingSenderId: "748977246196",
-  appId: "1:748977246196:web:a6609a4fc4fd0b1c9873a1",
-  measurementId: "G-TTNVNNS37D",
+  apiKey: 'AIzaSyAFgki31LNTHLuW8sutx-DZo6YAUXWo0pk',
+  authDomain: 'ecom-db-store.firebaseapp.com',
+  projectId: 'ecom-db-store',
+  storageBucket: 'ecom-db-store.appspot.com',
+  messagingSenderId: '748977246196',
+  appId: '1:748977246196:web:a6609a4fc4fd0b1c9873a1',
+  measurementId: 'G-TTNVNNS37D',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -32,18 +32,50 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
